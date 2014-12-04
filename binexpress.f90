747ed33e0e7ebdb6374233c5ad2e91b7cfd781c2
Iwaa Liu <iwaaliu@users.noreply.github.com>
2014-12-04 00:33:58 -0600

    Update binexpress.f90

--- binexpress.f90
+++ binexpress.f90
@@ -4,11 +4,11 @@
   bin=10
   write(A,*) bin
   print*, A
-  
+  print*, "Input a number "
   read(*,*) A
   n=log(A*1.)/log(2.)+1
   allocate(c(N))
-  print*,"N=", N
+  print*,"N=", N , "digits in total."
   ja=0
   j=1
   do while(a>0)
@@ -23,6 +23,6 @@ a=a/2
 j=j+1
 enddo
 
-print*, A,ja
+print*, "we have ",ja, "1 in this binary expression."
 print*, c(:)
 end program
